de6d13b1fbeccbe2597d5bf32afae63fa9b073b0
Kiran Shinde <kiranshinde@Kirans-MacBook-Air.local>
2024-03-10 16:50:42 +0000

    First Java Assignment

---
+++ FirstJavaAssignment/JavaFirstProg.java
@@ -0,0 +1,39 @@
+package JavaAutoFirstPro;
+
+public class JavaFirstProg 
+{
+
+	void add()
+	{
+		System.out.println("Addition is:");
+	}
+	
+	void sub()
+	{
+		System.out.println("Subtraction is:");
+	}
+	
+	static void mul()
+	{
+		System.out.println("Multiplication is:");
+	}
+	
+	static void div()
+	{
+		System.out.println("Division is:");
+	}
+	public static void main(String[] args)
+	{
+		
+		JavaFirstProg J1=new JavaFirstProg();
+		
+		J1.add();
+		
+		J1.sub();
+		
+		mul();
+		
+		div();
+	}
+
+}
