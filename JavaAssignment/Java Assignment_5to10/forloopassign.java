dbb7cd1e74ba7550ed28a0752f3fef07da3c5c8f
“Ashwini <ashwini.newase.47@gmail.com>
2024-03-18 11:08:15 +0000

    Java Assignment_5to10

---
+++ JavaAssignment/Java Assignment_5to10/forloopassign.java
@@ -0,0 +1,69 @@
+package JavaAutoFirstPro;
+
+public class forloopassign 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		
+		System.out.println("Practice Programme1 starts here");
+		for(byte i=1; i<=10; i++)
+		{
+			System.out.print(i+ "  ");
+			
+		}
+		
+		System.out.println();
+		System.out.println("Practice Programme1 ends here");
+		
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("Practice Programme2 starts here");
+		for(byte i=-10; i<=0; i++)
+		{
+			System.out.print(i+ "  ");
+		}
+		System.out.println();
+		System.out.println("Practice Programme2 ends here");
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("Practice Programme3 starts here");
+		for(byte i=-100; i>=-110; i--)
+		{
+			System.out.print(i+ "  ");
+		}
+		System.out.println();
+		System.out.println("Practice Programme3 ends here");
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("Practice Programme4 starts here");
+		for(byte i=0; i>=0;i++)
+		{
+			System.out.print(i+ "  ");
+		}
+		System.out.println();
+		System.out.println("Practice Programme4 ends here");
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("Practice Programme5 starts here");
+		for(byte i=0; i<=0;i--)
+		{
+			System.out.print(i+ "  ");
+		}
+		System.out.println();
+		System.out.println("Practice Programme5 ends here");
+		
+		
+
+	}
+
+}
