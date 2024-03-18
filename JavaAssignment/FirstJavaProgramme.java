641ea745576ec87becfe6e0a3cf51fea64ab5d17
“Ashwini <ashwini.newase.47@gmail.com>
2024-03-18 10:51:12 +0000

    Java First Assignment

---
+++ JavaAssignment/FirstJavaProgramme.java
@@ -0,0 +1,9 @@
+package JavaAutoAssignments;
+
+public class FirstJavaProgramme 
+{
+		public static void main(String[] args)
+		{
+			System.out.println("Hello, This is my first Java Programme!");
+		}
+}
