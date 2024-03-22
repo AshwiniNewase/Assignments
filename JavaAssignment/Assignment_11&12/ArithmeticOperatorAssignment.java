3c204ed42a1ee5749f63d5afb1b4447d13a77813
“Ashwini <ashwini.newase.47@gmail.com>
2024-03-22 17:19:22 +0000

    Java Assignment11 & 12

---
+++ JavaAssignment/Assignment_11&12/ArithmeticOperatorAssignment.java
@@ -0,0 +1,100 @@
+package JavaAutoFirstPro;
+
+public class ArithmeticOperatorAssignment
+{
+
+	public static void main(String[] args)
+	{
+		//Calculate area of rectangle
+		float length=5.50f;
+		float width=10.4f;
+		float AreaRect=length*width;
+		
+		System.out.println("Area of Rectangle is:" + AreaRect);
+		
+		System.out.println();
+		System.out.println();
+		
+		//Calculate area of triangle
+		float height=9.45f;
+		float base=12.57f;
+		float AreaTri=(height*base)/2;
+		
+		System.out.println("Area of Triangle is:" + AreaTri);
+		
+		System.out.println();
+		System.out.println();
+		
+		//Calculate area of square
+		float side=4.00f;
+		float AreaSqr=side*side;
+		
+		System.out.println("Area of Square is:" + AreaSqr);
+		
+		System.out.println();
+		System.out.println();
+		
+		//Calculate area of circle
+		float radius=7.35f;
+		float pi=3.14f;
+		float AreaCircle=pi*radius*radius;
+				
+		System.out.println("Area of Circle is:" + AreaCircle);
+				
+		System.out.println();
+		System.out.println();
+		
+		//Calculate circumference of rectangle
+		float l=28.00f;
+		float w=12.90f;
+		float CirRect=2*(l+w);
+		
+		System.out.println("Circumferece of Rectangle is:" + CirRect);
+		
+		System.out.println();
+		System.out.println();
+		
+		//Calculate Circumference of triangle
+		float s1=9.45f;
+		float s2=12.57f;
+		float s3=98.65f;
+		float CirTri=s1+s2+s3;
+				
+		System.out.println("Circumference of Triangle is:" + CirTri);
+				
+		System.out.println();
+		System.out.println();
+		
+		
+		//Calculate Circumference of square
+		float s=87.00f;
+		float CirSqr=2*side;
+				
+		System.out.println("Circumference of Square is:" + CirSqr);
+				
+		System.out.println();
+		System.out.println();
+		
+		
+		//Calculate circumference of circle
+		float r=17.35f;
+		float Pi=3.14f;
+		float CirCircle=2*Pi*r;
+						
+		System.out.println("Circumference of Circle is:" + CirCircle);
+		
+		System.out.println();
+		System.out.println();
+		
+				
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
